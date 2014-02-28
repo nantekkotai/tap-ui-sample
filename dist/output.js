bc9c9f7e156dae2a413d160fce1d5f1d257de1e6
Nobuyuki OSAWA <n-osawa@mapion.co.jp>
2014-02-28 19:00:41 +0900

    throttle機能追加

---
+++ dist/output.js
@@ -0,0 +1,84 @@
+function Star(elm, addFunc, removeFunc) {
+  if (!elm) return;
+  elm = $(elm);
+
+  var offset = elm.offset(),
+      size = { width: elm.innerWidth(), height: elm.innerHeight() },
+      effectiveRange = {
+        minX: offset.left,
+        maxX: offset.left + size.width,
+        minY: offset.top,
+        maxY: offset.top + size.height
+      };
+
+  function isEffective(x, y) {
+    return !!(effectiveRange.minX <= x
+            && x <= effectiveRange.maxX
+            && effectiveRange.minY <= y 
+            && y <= effectiveRange.maxY);
+  }
+
+  function throttle(ms) {
+    var _before,
+        timerId;
+
+    return function (func) {
+      _before = func;
+      if (timerId) {
+        clearTimeout(timerId);
+        delete timerId;
+      }
+      timerId = setTimeout(_before, ms);
+    }
+  }
+  var thr = throttle(200);
+
+  elm
+  .bind('touchstart', function () {
+     $(this).addClass('on');
+  })
+  .bind('touchmove', function (e) {
+    if (isEffective(event.changedTouches[0].pageX, event.changedTouches[0].pageY)) {
+       $(this).addClass('on');
+    } else {
+       $(this).removeClass('on');
+    }
+  })
+  .bind('touchend', function () {
+    $(this).removeClass('on');
+    if (!isEffective(event.changedTouches[0].pageX, event.changedTouches[0].pageY)) return;
+
+    var $star = $($(this).find('i'));
+    if ($star.hasClass('fa-star-o')) {
+      $star.removeClass('fa-star-o').addClass('fa-star');
+      if (addFunc && typeof addFunc == 'function') {
+        thr(addFunc);
+      }
+
+    } else {
+      $star.removeClass('fa-star').addClass('fa-star-o');
+      if (removeFunc && typeof removeFunc == 'function') {
+        thr(removeFunc);
+      }
+    }
+  });
+
+  this.elm = elm;
+}
+Star.prototype = {
+  check: function () {
+
+  },
+
+  toggle: function () {
+
+  },
+
+  on: function () {
+
+  },
+
+  off: function () {
+
+  }
+}
